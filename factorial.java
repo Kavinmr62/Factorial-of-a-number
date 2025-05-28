63c21f5ab2f823f9bbd785c8ce815d5190e1d8b3
Kavin M R <154052561+Kavinmr62@users.noreply.github.com>
2025-05-28 07:00:25 +0530

    Update and rename README.md to factorial.java

---
+++ factorial.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int p=1;
+		for(int i=1;i<=n;i++){
+		    p*=i;
+		}
+		String s=Integer.toString(p);
+		for(int i=0;i<s.length();i++){
+		    System.out.print(s.charAt(i)+" ");
+		}
+	}
+}
